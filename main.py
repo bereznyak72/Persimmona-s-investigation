4dcb4d99cfee7f6db883e87780809be8c7752560
Владислав Конкин <149203870+bereznyak72@users.noreply.github.com>
2025-03-08 15:59:09 +0500

    Delete unnecessary code

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import pygame
-from levels.level1 import Level1
-from levels.level2 import Level2
-from levels.level3 import Level3
-from levels.level4 import Level4
-from levels.level5 import Level5
+from levels import Level1, Level2, Level3, Level4, Level5
+
 
 def main():
     pygame.init()
--- utils/helpers.py
+++
@@ -1,2 +0,0 @@
-def load_image(path):
-    return pygame.image.load(path)
