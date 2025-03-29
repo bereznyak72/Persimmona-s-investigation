df5154bc722c743d1d6bdf4828970140362148aa
Vlad Kostyukovich <mister.costiukovitch@yandex.ru>
2025-03-29 18:40:31 +0500

    Translate prologue

--- levels/prologue.py
+++ levels/prologue.py
@@ -8,7 +8,6 @@ class Prologue:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.font = pygame.font.Font('assets/fonts/Persimmona.ttf', int(self.screen_height * 0.035))
-        #self.font = pygame.font.SysFont("Arial", int(self.screen_height * 0.035), bold=False)
         self.text_animation_speed = 15
         self.last_char_time = 0
         self.current_text = ""
@@ -22,15 +21,15 @@ class Prologue:
         self.char_index = 0
         self.text_lines = []
         self.presenter_text = (
-            "Добрый вечер, уважаемые дамы и господа!",
-            "Вы смотрите шоу о самых известных жителях нашей страны.",
-            "Сегодня у нас в гостях самый известный детектив — Персиммона!",
+            "Good evening, ladies and gentlemen!",
+            "You are watching a show about the most famous inhabitants of our country.",
+            "Our guest today is the most famous detective, Persimmona.",
         )
-        self.presenter_question = ("Персиммона, скажите, как же вы обрели такую известность?",)
+        self.presenter_question = ("Persimmona, tell me, how did you become so famous?",)
         self.persimmona_text = (
-            "О, да, я точно помню день, когда это произошло,",
-            "это было одно из самых интересных расследований в моей жизни!",
-            "Вообщем, дело было так..."
+            "Oh, yes, I remember exactly the day it happened,",
+            "It was one of the most interesting investigations of my life!",
+            "in general, it was like this..."
         )
         self.start_text_animation(self.presenter_text)
         self.text_area_height = 150
@@ -40,7 +39,6 @@ class Prologue:
         reference_width, reference_height = 1920, 1080
         target_persimmona_width, target_persimmona_height = 316, 606
         target_presenter_width, target_presenter_height = 516, 516
-        #target_background_width, target_background_height = self.screen_width, self.screen_height
         width_scale = self.screen_width / reference_width
         height_scale = self.screen_height / reference_height
         scale_factor = min(width_scale, height_scale)
@@ -49,15 +47,15 @@ class Prologue:
         new_presenter_width = max(int(target_presenter_width * scale_factor), 50)
         new_presenter_height = max(int(target_presenter_height * scale_factor), 96)
         new_background_width = self.screen_width
-        new_background_height = self.screen_height - 200 # Костыль
+        new_background_height = self.screen_height - 200
         self.persimmona_image = pygame.transform.scale(self.persimmona_image, (new_persimmona_width, new_persimmona_height))
         self.presenter_image = pygame.transform.scale(self.presenter_image, (new_presenter_width, new_presenter_height))
         self.background_image = pygame.transform.scale(self.background_image, (new_background_width, new_background_height))       
         self.background_rect = pygame.Rect(0, 0, self.screen_width, self.screen_height)
         self.persimmona_rect = pygame.Rect(self.screen_width - new_persimmona_width, self.screen_height - new_persimmona_height, new_persimmona_width, new_persimmona_height) # Работает только с максимальным разрешением
         self.presenter_rect = pygame.Rect(self.screen_width - new_presenter_width + 120, self.screen_height - new_presenter_height + 100, new_presenter_width, new_presenter_height) # Работает только с максимальным разрешением
-        self.persimmona_text_max_width = self.screen_width - new_persimmona_width - 60 # Работает только с максимальным разрешением
-        self.presenter_text_max_width = self.screen_width - new_presenter_width - 60 # Работает только с максимальным разрешением
+        self.persimmona_text_max_width = self.screen_width - new_persimmona_width - 60
+        self.presenter_text_max_width = self.screen_width - new_presenter_width - 60
 
 
 
@@ -106,7 +104,7 @@ class Prologue:
 
 
     def render_text(self, screen, text, y_pos, center=False):
-        lines = self.wrap_text(text, self.presenter_text_max_width - 75) # Костыль
+        lines = self.wrap_text(text, self.presenter_text_max_width - 75)
         for i, line in enumerate(lines):
             text_surface = self.font.render(line, True, (255, 255, 255))
             if center:
